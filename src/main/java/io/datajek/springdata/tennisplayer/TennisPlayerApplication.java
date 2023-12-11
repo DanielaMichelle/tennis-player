4df6f6b0da40d0462cb8b079b92a603c9428135e
Daniela Infantes <danielamij251@gmail.com>
2023-12-11 14:55:08 -0500

    SELECT Query, Defining Player bean, Creating DAO class, getAllPlayers() and getPlayerById() in DAO class.

--- src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
+++ src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
@@ -1,14 +1,29 @@
 package io.datajek.springdata.tennisplayer;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class TennisPlayerApplication {
+public class TennisPlayerApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PlayerDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TennisPlayerApplication.class, args);
 		System.out.println("hellooo");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All Players Data: {} ", dao.getAllPlayers());
+		logger.info("Player with Id 3: {}", dao.getPlayerById(3));
+	}
 }
